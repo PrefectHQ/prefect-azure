87589f60e22e5b4c767ed9923b56c88a6f96fcea
ahuang11 <andrew.h@prefect.io>
2022-02-26 18:01:09 -0700

    Add cosmos db credentials

--- prefect_azure/credentials.py
+++ prefect_azure/credentials.py
@@ -1,19 +1,22 @@
 from dataclasses import dataclass
 
+from azure.cosmos import CosmosClient
 from azure.storage.blob.aio import BlobServiceClient
 
-HELP_URL = (
-    "https://docs.microsoft.com/en-us/azure/storage/blobs/"
-    "storage-quickstart-blobs-python#copy-your-credentials-from-the-azure-portal"
-)
+HELP_URLS = {
+    "blob_service": "https://docs.microsoft.com/en-us/azure/storage/blobs/"
+    "storage-quickstart-blobs-python#copy-your-credentials-from-the-azure-portal",
+    "cosmos": "https://docs.microsoft.com/en-us/azure/cosmos-db/sql/"
+    "create-sql-api-python#update-your-connection-string",
+}
 
 
 @dataclass
 class AzureCredentials:
-    f"""
+    """
     Dataclass used to manage authentication with Azure. Azure authentication is
-    handled via the `azure` module. Refer to the [azure docs]({HELP_URL})
-    for more info about retrieving the connection string.
+    handled via the `azure` module, primarily through a connection string.
+
     Args:
         connection_string
     """
@@ -23,11 +26,12 @@ class AzureCredentials:
     def get_blob_service_client(self) -> BlobServiceClient:
         """
         Returns an authenticated blob service client that can be used to create clients
-        for Azure services
+        for Azure services.
+
         Example:
             Create an authorized blob service session
             ```python
-            connection_string = Secret("azure").get()
+            connection_string = Secret("azure_blob_service").get()
             azure_credentials = AzureCredentials(
                 connection_string=connection_string,
             )
@@ -39,5 +43,30 @@ class AzureCredentials:
             return BlobServiceClient.from_connection_string(connection_string)
         except ValueError:
             raise ValueError(
-                f"Please visit {HELP_URL} for retrieving the proper connection string."
+                f"Please visit {HELP_URLS['blob_service']} for retrieving "
+                f"the proper connection string."
+            )
+
+    def get_cosmos_client(self) -> CosmosClient:
+        """
+        Returns an authenticated Cosmos client that can be used to query databases
+        for Azure services.
+
+        Example:
+            Create an authorized Cosmos session
+            ```python
+            connection_string = Secret("azure_cosmos").get()
+            azure_credentials = AzureCredentials(
+                connection_string=connection_string,
+            )
+            cosmos_client = azure_credentials.get_cosmos_client()
+            ```
+        """
+        connection_string = self.connection_string
+        try:
+            return CosmosClient.from_connection_string(connection_string)
+        except ValueError:
+            raise ValueError(
+                f"Please visit {HELP_URLS['cosmos']} for retrieving "
+                f"the proper connection string."
             )
