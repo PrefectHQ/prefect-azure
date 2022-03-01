92448df091f0ffacb61ad25b6a7987cc9015b774
Andrew <15331990+ahuang11@users.noreply.github.com>
2022-03-01 12:01:38 -0700

    Remove __all__

--- prefect_azure/__init__.py
+++ prefect_azure/__init__.py
@@ -1,6 +1,4 @@
 from . import _version
 from .credentials import BlobStorageAzureCredentials, CosmosDbAzureCredentials  # noqa
 
-__all__ = ["BlobStorageAzureCredentials", "CosmosDbAzureCredentials"]
-
 __version__ = _version.get_versions()["version"]
