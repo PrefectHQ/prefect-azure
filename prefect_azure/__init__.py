af931b093202a62649bcd0571674140f1797bf59
ahuang11 <andrew.h@prefect.io>
2022-03-04 09:14:02 -0700

    Add ML Datastore

--- prefect_azure/__init__.py
+++ prefect_azure/__init__.py
@@ -2,6 +2,7 @@ from . import _version
 from .credentials import (  # noqa
     BlobStorageAzureCredentials,
     CosmosDbAzureCredentials,
+    MlAzureCredentials,
 )
 
 __version__ = _version.get_versions()["version"]
--- prefect_azure/credentials.py
+++ prefect_azure/credentials.py
@@ -17,11 +17,18 @@ try:
 except ModuleNotFoundError:
     pass  # a descriptive error will be raised in get_client
 
+try:
+    from azureml.core.authentication import ServicePrincipalAuthentication
+    from azureml.core.workspace import Workspace
+except ModuleNotFoundError:
+    pass  # a descriptive error will be raised in get_client
+
 from prefect.logging import get_run_logger
 
 HELP_URLS = {
     "blob_storage": "https://docs.microsoft.com/en-us/azure/storage/blobs/storage-quickstart-blobs-python#copy-your-credentials-from-the-azure-portal",  # noqa
     "cosmos_db": "https://docs.microsoft.com/en-us/azure/cosmos-db/sql/create-sql-api-python#update-your-connection-string",  # noqa
+    "ml_datastore": "https://github.com/Azure/MachineLearningNotebooks/blob/master/how-to-use-azureml/manage-azureml-service/authentication-in-azureml/authentication-in-azureml.ipynb",  # noqa
 }
 HELP_FMT = "Please visit {help_url} for retrieving the proper connection string."
 
@@ -280,3 +287,68 @@ class CosmosDbAzureCredentials:
         database_client = self.get_database_client(database)
         container_client = database_client.get_container_client(container=container)
         return container_client
+
+
+@dataclass
+class MlAzureCredentials:
+    """
+    Dataclass used to manage authentication with Azure. Azure authentication is
+    handled via the `azure` module.
+    Args:
+        tenant_id: The active directory tenant that the service identity belongs to.
+        service_principal_id: The service principal ID.
+        service_principal_password: The service principal password/key.
+        subscription_id: The Azure subscription ID containing the workspace.
+        resource_group: The resource group containing the workspace.
+        workspace_name: The existing workspace name.
+        connection_string: Used for registering a blob container to a datastore.
+    """
+
+    tenant_id: str
+    service_principal_id: str
+    service_principal_password: str
+    subscription_id: str
+    resource_group: str
+    workspace_name: str
+    connection_string: str = None
+
+    @_raise_help_msg("ml_datastore")
+    def get_client(self) -> "Workspace":
+        """
+        Returns an authenticated base Workspace that can be used in
+        Azure's Datasets and Datastores.
+        Example:
+            Create an authorized Blob Service session
+            ```python
+            import os
+            from prefect import flow
+            from prefect_azure import MlAzureCredentials
+            @flow
+            def example_get_client_flow():
+                azure_credentials = MlAzureCredentials(
+                    "tenant_id",
+                    "service_principal_id",
+                    "service_principal_password",
+                    "subscription_id",
+                    "resource_group",
+                    "workspace_name"
+                )
+                workspace_client = azure_credentials.get_client()
+                return workspace_client
+            example_get_client_flow()
+            ```
+        """
+        service_principal_authentication = ServicePrincipalAuthentication(
+            tenant_id=self.tenant_id,
+            service_principal_id=self.service_principal_id,
+            service_principal_password=self.service_principal_password,
+        )
+
+        workspace = Workspace(
+            subscription_id=self.subscription_id,
+            resource_group=self.resource_group,
+            workspace_name=self.workspace_name,
+            auth=service_principal_authentication,
+        )
+
+        return workspace
