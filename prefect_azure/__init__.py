0535d31f3a28682ad4c9f054bec86e937a354db1
Ryan Peden <ryan@rpeden.com>
2022-09-25 22:35:30 -0400

    Added Azure Container Instances classes to module init

--- prefect_azure/__init__.py
+++ prefect_azure/__init__.py
@@ -3,6 +3,10 @@ from .credentials import (  # noqa
     AzureBlobStorageCredentials,
     AzureCosmosDbCredentials,
     AzureMlCredentials,
+    ACICredentials
 )
+from .aci import ACITask
+
+__all__ = ["AzureBlobStorageCredentials", "AzureCosmosDbCredentials", "AzureMlCredentials", "ACICredentials", "ACITask"]
 
 __version__ = _version.get_versions()["version"]
