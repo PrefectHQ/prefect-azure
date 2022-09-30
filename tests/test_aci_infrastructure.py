a8a7e2c63acdff818f5684453e55b6f97e48d0c3
Ryan Peden <ryan@rpeden.com>
2022-09-29 23:47:49 -0400

    Added more tests

--- tests/test_aci_infrastructure.py
+++ tests/test_aci_infrastructure.py
@@ -1,10 +1,11 @@
-from typing import Tuple
+import json
+from typing import Tuple, Union
 from unittest.mock import MagicMock, Mock
 
 import pytest
+from anyio.abc import TaskStatus
 from azure.core.exceptions import HttpResponseError
 from azure.identity import ClientSecretCredential
-from azure.mgmt.containerinstance.operations import ContainerGroupsOperations
 from azure.mgmt.resource import ResourceManagementClient
 
 import prefect_azure.container_instance
@@ -12,6 +13,7 @@ from prefect_azure import ContainerInstanceCredentials
 from prefect_azure.container_instance import (
     ContainerGroupProvisioningState,
     ContainerInstanceJob,
+    ContainerInstanceJobResult,
 )
 
 
@@ -37,7 +39,7 @@ def credential_values(
 
 
 @pytest.fixture()
-def aci_block():
+def container_instance_block():
     """
     Returns a basic initialized ACI infrastructure block suitable for use
     in a variety of tests.
@@ -50,19 +52,21 @@ def aci_block():
         client_id=client_id, client_secret=client_secret, tenant_id=tenant_id
     )
 
-    aci_block = ContainerInstanceJob(
+    container_instance_block = ContainerInstanceJob(
         command=["test"],
         aci_credentials=credentials,
         azure_resource_group_name="testgroup",
         subscription_id="subid",
     )
 
-    return aci_block
+    return container_instance_block
 
 
 @pytest.fixture()
 def mock_aci_client(monkeypatch, mock_resource_client):
-
+    """
+    A fixture that provides a mock Azure Container Instances client
+    """
     mock_aci_client = Mock()
     mock_container_groups = Mock()
     mock_aci_client.container_groups.return_value = mock_container_groups
@@ -73,15 +77,29 @@ def mock_aci_client(monkeypatch, mock_resource_client):
     return mock_aci_client
 
 
-@pytest.fixture()
-def mock_container_groups(monkeypatch):
-    container_groups = MagicMock(spec=ContainerGroupsOperations)
-    monkeypatch.setattr(
-        "azure.mgmt.containerinstance.operations.ContainerGroupsOperations",
-        MagicMock(return_value=container_groups),
-    )
+def create_mock_container_group(state: str, exit_code: Union[int, None]):
+    container_group = Mock()
+    containers = MagicMock()
+    container = Mock()
+    container.instance_view.current_state.state = state
+    container.instance_view.current_state.exit_code = exit_code
+    containers.__getitem__.return_value = container
+    container_group.containers = containers
+    return container_group
 
-    return container_groups
+
+@pytest.fixture()
+def mock_successful_container_group():
+    """
+    A fixture that returns a mock container group that mimics a successfully
+    provisioned container group returned from Azure.
+    """
+    return create_mock_container_group(state="Terminated", exit_code=0)
+
+
+@pytest.fixture()
+def mock_running_container_group():
+    return create_mock_container_group(state="Running", exit_code=None)
 
 
 @pytest.fixture()
@@ -131,8 +149,10 @@ def test_invalid_command_validation():
 
 
 @pytest.mark.usefixtures("mock_aci_client")
-def test_credentials_are_used(aci_block: ContainerInstanceJob, monkeypatch):
-    credentials = aci_block.aci_credentials
+def test_credentials_are_used(
+    container_instance_block: ContainerInstanceJob, monkeypatch
+):
+    credentials = container_instance_block.aci_credentials
     (client_id, client_secret, tenant_id) = credential_values(credentials)
 
     mock_client_id = Mock(return_value=client_id)
@@ -149,7 +169,7 @@ def test_credentials_are_used(aci_block: ContainerInstanceJob, monkeypatch):
         prefect_azure.container_instance, "ClientSecretCredential", mock_credential
     )
 
-    aci_block.run()
+    container_instance_block.run()
 
     mock_client_id.assert_called_once()
     mock_client_secret.assert_called_once()
@@ -159,39 +179,45 @@ def test_credentials_are_used(aci_block: ContainerInstanceJob, monkeypatch):
     )
 
 
-def test_container_creation_call(mock_aci_client, aci_block):
+def test_container_creation_call(mock_aci_client, container_instance_block):
     # ensure the block always tries to call the Azure SDK to create the container
-    aci_block.run()
+    container_instance_block.run()
     mock_aci_client.container_groups.begin_create_or_update.assert_called_once()
 
 
-def test_delete_after_group_creation_failure(aci_block, mock_aci_client, monkeypatch):
+def test_delete_after_group_creation_failure(
+    container_instance_block, mock_aci_client, monkeypatch
+):
     # if provisioning failed, the container group should be deleted
     mock_container_group = Mock()
     mock_container_group.provisioning_state.return_value = (
         ContainerGroupProvisioningState.FAILED
     )
     monkeypatch.setattr(
-        aci_block, "_wait_for_task_container_start", mock_container_group
+        container_instance_block, "_wait_for_task_container_start", mock_container_group
     )
-    aci_block.run()
+    container_instance_block.run()
     mock_aci_client.container_groups.begin_delete.assert_called_once()
 
 
-def test_delete_after_group_creation_success(aci_block, mock_aci_client, monkeypatch):
+def test_delete_after_group_creation_success(
+    container_instance_block, mock_aci_client, monkeypatch
+):
     # if provisioning was successful, the container group should eventually be deleted
     mock_container_group = Mock()
     mock_container_group.provisioning_state.return_value = (
         ContainerGroupProvisioningState.SUCCEEDED
     )
     monkeypatch.setattr(
-        aci_block, "_wait_for_task_container_start", mock_container_group
+        container_instance_block, "_wait_for_task_container_start", mock_container_group
     )
-    aci_block.run()
+    container_instance_block.run()
     mock_aci_client.container_groups.begin_delete.assert_called_once()
 
 
-def test_delete_after_after_exception(aci_block, mock_aci_client, monkeypatch):
+def test_delete_after_after_exception(
+    container_instance_block, mock_aci_client, monkeypatch
+):
     # if an exception was thrown while waiting for container group provisioning,
     # the group should be deleted
     mock_aci_client.container_groups.begin_create_or_update.side_effect = (
@@ -199,6 +225,121 @@ def test_delete_after_after_exception(aci_block, mock_aci_client, monkeypatch):
     )
 
     with pytest.raises(HttpResponseError):
-        aci_block.run()
+        container_instance_block.run()
         mock_aci_client.container_groups.begin_create_or_update.assert_called_once()
         mock_aci_client.container_groups.begin_delete.assert_called_once()
+
+
+@pytest.mark.usefixtures("mock_aci_client")
+def test_task_status_started_on_provisioning_success(
+    container_instance_block, mock_successful_container_group, monkeypatch
+):
+    monkeypatch.setattr(
+        container_instance_block, "_provisioning_succeeded", Mock(return_value=True)
+    )
+
+    monkeypatch.setattr(
+        container_instance_block,
+        "_wait_for_task_container_start",
+        Mock(return_value=mock_successful_container_group),
+    )
+
+    task_status = Mock(spec=TaskStatus)
+    container_instance_block.run(task_status=task_status)
+
+    task_status.started.assert_called_once()
+
+
+@pytest.mark.usefixtures("mock_aci_client")
+def test_task_status_not_started_on_provisioning_failure(
+    container_instance_block, mock_successful_container_group, monkeypatch
+):
+    monkeypatch.setattr(
+        container_instance_block, "_provisioning_succeeded", Mock(return_value=False)
+    )
+
+    task_status = Mock(spec=TaskStatus)
+    container_instance_block.run(task_status=task_status)
+    task_status.started.assert_not_called()
+
+
+def test_provisioning_timeout_throws_exception(
+    mock_aci_client, container_instance_block
+):
+    mock_poller = Mock()
+    mock_poller.done.return_value = False
+    mock_aci_client.container_groups.begin_create_or_update.return_value = mock_poller
+
+    # avoid delaying test runs
+    container_instance_block.task_watch_poll_interval = 0.02
+    container_instance_block.task_start_timeout_seconds = 0.10
+
+    with pytest.raises(RuntimeError):
+        container_instance_block.run()
+
+
+def test_watch_for_container_termination(
+    mock_aci_client,
+    mock_running_container_group,
+    mock_successful_container_group,
+    container_instance_block,
+    monkeypatch,
+):
+    monkeypatch.setattr(
+        container_instance_block, "_provisioning_succeeded", Mock(return_value=True)
+    )
+    monkeypatch.setattr(
+        container_instance_block,
+        "_wait_for_task_container_start",
+        Mock(return_value=mock_running_container_group),
+    )
+
+    # make the block wait a few times before we give it a successful result
+    # so we can make sure the watcher actually watches instead of skipping
+    # the timeout
+    run_count = 0
+
+    def get_container_group(**kwargs):
+        nonlocal run_count
+        run_count += 1
+        if run_count < 5:
+            return mock_running_container_group
+        else:
+            return mock_successful_container_group
+
+    mock_aci_client.container_groups.get.side_effect = get_container_group
+
+    container_instance_block.task_watch_poll_interval = 0.02
+    result = container_instance_block.run()
+
+    # ensure the watcher was watching
+    assert run_count == 5
+    assert mock_aci_client.container_groups.get.call_count == run_count
+    # ensure the run completed
+    assert isinstance(result, ContainerInstanceJobResult)
+
+
+def test_preview():
+    # ensures the preview generates the JSON expected
+    block_args = {
+        "azure_resource_group_name": "test-group",
+        "memory": 1.0,
+        "cpu": 1.0,
+        "gpu_count": 1,
+        "gpu_sku": "V100",
+        "env": {"FAVORITE_ANIMAL": "cat"},
+    }
+
+    block = ContainerInstanceJob(**block_args)
+
+    preview = json.loads(block.preview())
+
+    for (k, v) in block_args.items():
+        if k == "env":
+            for (k2, v2) in block_args["env"].items():
+                assert preview[k][k2] == block_args["env"][k2]
+
+            assert "PREFECT_API_URL" in preview[k]
+
+        else:
+            assert preview[k] == block_args[k]
