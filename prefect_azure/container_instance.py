e23a773668134d14fc9ca019f833288f8c54cd32
Ryan Peden <ryan@rpeden.com>
2022-11-28 13:11:09 -0500

    Updated ACI infrastructure block to treat PREFECT_API_KEY as secure

--- prefect_azure/container_instance.py
+++ prefect_azure/container_instance.py
@@ -87,6 +87,9 @@ ACI_DEFAULT_CPU = 1.0
 ACI_DEFAULT_MEMORY = 1.0
 ACI_DEFAULT_GPU = 0.0
 DEFAULT_CONTAINER_ENTRYPOINT = "/opt/prefect/entrypoint.sh"
+# environment variables that ACI should treat as secure variables so they
+# won't appear in logs
+ENV_SECRETS = ["PREFECT_API_KEY"]
 
 
 class ContainerGroupProvisioningState(str, Enum):
@@ -328,9 +331,12 @@ class AzureContainerInstanceJob(Infrastructure):
 
         # setup container environment variables
         environment = [
-            EnvironmentVariable(name=k, value=v)
+            EnvironmentVariable(name=k, secure_value=v)
+            if k in ENV_SECRETS
+            else EnvironmentVariable(name=k, value=v)
             for (k, v) in self._get_environment().items()
         ]
+
         # all container names in a resource group must be unique
         container_name = str(uuid.uuid4())
         container_resource_requirements = self._configure_container_resources()
